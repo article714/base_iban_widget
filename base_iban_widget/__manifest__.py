a488cf022055da58187018b0d0f7665d858d6628
Damien Couppé <5243814+Kontrolix@users.noreply.github.com>
2020-03-16 11:56:18 +0100

    [IMP] Move to dir base_iban_widget

---
+++ base_iban_widget/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "IBAN Bank Accounts widget",
+    "category": "Tools",
+    "description": """Backport of base_iban widget from v13 to v12""",
+    "depends": ["base_iban"],
+    "data": ["views/templates.xml", "views/partner_view.xml"],
+    "license": "LGPL-3",
+}
